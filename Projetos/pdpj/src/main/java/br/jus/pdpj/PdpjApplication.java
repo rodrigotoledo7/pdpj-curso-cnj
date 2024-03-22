a83a234285a8c21e8bb27b73ae6e4b769537aab5
Rodrigo Toledo <rodrigotoledo@tjro.jus.br>
2024-03-22 10:34:53 -0400

    first commit

---
+++ Projetos/pdpj/src/main/java/br/jus/pdpj/PdpjApplication.java
@@ -0,0 +1,13 @@
+package br.jus.pdpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdpjApplication.class, args);
+	}
+
+}
---
+++ Projetos/pdpj/src/test/java/br/jus/pdpj/PdpjApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.pdpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
